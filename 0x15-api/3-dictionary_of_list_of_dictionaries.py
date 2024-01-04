13d293f395c01f701775126808e0671faf1844e9
Stacy-Muhia <stacymuhiap@gmail.com>
2024-01-04 01:35:47 -0800

    corrected indentation

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,11 +7,12 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
-
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "username": u.get("username"),
+        json.dump(
+            {
+                u.get("id"): [
+                    {
+                        "username": u.get("username"),
                         "task": task.get("title"),
                         "completed": task.get("completed")
                     }
